bbd1ba20968b597bf310d170186af9a72eb8658a
Al Amin <alamin>
2023-05-26 12:49:39 +0600

    background shape fixed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,14 +3,14 @@ import img from '../../../assets/banner/banner.png'
 
 const Banner = () => {
   return (
-    <div className='bg-[#FAFEFE] py-8 banner-shape'>
-        <div className="container shape">
+    <div className='bg-[#FAFEFE] banner-shape'>
+        <div className="container py-8">
             <div className="flex items-center justify-between">
                 <div>
                     <img src={img} className='max-w-[553px] w-full' alt="" />
                 </div>
                 <div>
-                    <h2 className='heading font-Jost font-semibold text-7xl'>Your concept, our <br /> creativity</h2>
+                    <h2 className='heading font-Jost font-semibold lg:text-6xl text-5xl'>Your concept, our <br className='hidden lg:block' /> creativity</h2>
                     <p className='mt-5 mb-12 font-Jost text-base text-[#444444]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
                     <button className="btn">Get Started</button>
                 </div>
